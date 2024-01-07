19dbe9c7a9a588f25ce1a6bdfe94981e893814f2
Raphael <raphael.antunes@sou.inteli.edu.br>
2024-01-07 03:24:05 -0300

    Cash view

--- Front/Home/home.js
+++ Front/Home/home.js
@@ -1,4 +1,36 @@
-let name = 'Raphael'
-let welcome = document.getElementById('welcome')
+const getName = () => {
+    let name = 'Raphael'
+    let welcome = document.getElementById('welcome')
+    
+    welcome.innerHTML = 'Seja bem-vindo <strong>' + name + '</strong>'
+}
 
-welcome.innerHTML = 'Seja bem-vindo <strong>' + name + '</strong>'
+getName()
+
+
+const showCredit = () => {
+    let credit_qtd = '1000,00'
+    let credit = document.getElementById('credit')
+
+    credit.innerHTML = 'R$ ' + credit_qtd
+}
+
+showCredit()
+
+const showDebit = () => {
+    let debit_qtd = '1000,00'
+    let debit = document.getElementById('debit')
+
+    debit.innerHTML = 'R$ ' + debit_qtd
+}
+
+showDebit()
+
+const showTotal = () => {
+    let total_qtd = '1000,00'
+    let total = document.getElementById('total')
+
+    total.innerHTML = 'R$ ' + total_qtd
+}
+
+showTotal()
