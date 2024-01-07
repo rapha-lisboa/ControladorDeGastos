efc111b5def0ff62a779390f22d65313d070a927
Raphael <raphael.antunes@sou.inteli.edu.br>
2024-01-07 16:06:22 -0300

    Update nos numeros das contas

--- Front/Home/home.js
+++ Front/Home/home.js
@@ -5,32 +5,33 @@ const getName = () => {
     welcome.innerHTML = 'Seja bem-vindo <strong>' + name + '</strong>'
 }
 
-getName()
-
-
 const showCredit = () => {
-    let credit_qtd = '1000,00'
+    let credit_qtd = 1020.04
     let credit = document.getElementById('credit')
 
-    credit.innerHTML = 'R$ ' + credit_qtd
+    credit.innerHTML = credit_qtd.toFixed(2)
 }
+
+const showDebit = () => {
+    let debit_qtd = 1200.10
+    let debit = document.getElementById('debit')
+    
+    debit.innerHTML = debit_qtd.toFixed(2)
+}
+
+const showTotal = () => {
+    let credit_qtd = document.getElementById('credit')
+    let debit_qtd = document.getElementById('debit')
+    let total = document.getElementById('total')
+    let total_qtd = parseFloat(credit_qtd.textContent) + parseFloat(debit_qtd.textContent)
+    
+    total.innerHTML = total_qtd.toFixed(2)
+}
+
+getName()
 
 showCredit()
 
-const showDebit = () => {
-    let debit_qtd = '1000,00'
-    let debit = document.getElementById('debit')
-
-    debit.innerHTML = 'R$ ' + debit_qtd
-}
-
 showDebit()
-
-const showTotal = () => {
-    let total_qtd = '1000,00'
-    let total = document.getElementById('total')
-
-    total.innerHTML = 'R$ ' + total_qtd
-}
 
 showTotal()
