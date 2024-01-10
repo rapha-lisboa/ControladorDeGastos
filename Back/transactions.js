b20b69d7d72f157b1c37ab507a1d548ebd44696e
Raphael <raphael.antunes@sou.inteli.edu.br>
2024-01-10 01:12:26 -0300

    Conclusões funções back

--- Back/transactions.js
+++ Back/transactions.js
@@ -1,13 +1,12 @@
 const fs = require('fs')
-const chalk = require('chalk')
 
 // Adiciona uma transação
-const add = (description, value, type) => {
-    const transactions = loadTransactions()
+const add = (description, amount, type) => {
+    const transactions = readAllTransactions()
 
     const new_transaction = {
         description: description,
-        value: value,
+        amount: amount,
         type: type
     }
 
@@ -17,17 +16,21 @@ const add = (description, value, type) => {
 }
 
 // Lê 1 transação específica
-const readOne = (description) => {
-    const transactions = loadreTransactions()
+const readOneTransaction = (description) => {
+    const transactions = readAllTransactions()
 
     const the_chosen = transactions.find((element) => element.description === description)
 
     if(the_chosen){
-        
+        console.log('Transação encontrada com sucesso')
+        return JSON.stringify(the_chosen)
+    } else {
+        console.log('Esta transação não existe')
     }
 }
 
-const loadTransactions = () => {
+// Lê todas as transações
+const readAllTransactions = () => {
     try {
         let dataBuffer = fs.readFileSync('transactions.json')
         let dataJSON = dataBuffer.toString()
@@ -37,6 +40,35 @@ const loadTransactions = () => {
     } catch (e) {
         return []
     }
+}
+
+// Atualiza os valores de uma transação específica
+const updateTransaction = (old_description, new_description, new_amount, new_type) => {
+    let transactions = readAllTransactions()
+
+    let transaction_id = transactions.findIndex((element) => element.description === old_description)
+
+    transactions[transaction_id].description = new_description
+    transactions[transaction_id].amount = new_amount
+    transactions[transaction_id].type = new_type
+    
+    saveTransactions(transactions)
+}
+
+// Deleta uma transação
+const deleteTransaction = (description) => {
+    const transactions = readAllTransactions()
+
+    const new_transactions = transactions.filter((element) => element.description !== description)
+
+    if(transactions.length !== new_transactions.length){
+        console.log('Transaction removed successfuly')
+        saveTransactions(new_transactions)
+    } else {
+        console.log('Transaction description not founded')
+    }
+
+    return transactions.length !== new_transactions.length
 }
 
 const saveTransactions = (transactions) => {
